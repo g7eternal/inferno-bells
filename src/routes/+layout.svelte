dd5d7f48c911127922a7471e484bfde852c3ff0d
g7eternal <40625769+g7eternal@users.noreply.github.com>
2025-06-09 18:32:25 +0300

    prettify

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,14 @@
 <script>
-	import { ModeWatcher } from 'mode-watcher';
-	import '../app.css';
+  import { ModeWatcher } from "mode-watcher";
+  import "../app.css";
 
-	let { children } = $props();
+  let { children } = $props();
 </script>
 
 <svelte:head>
-	<link rel="apple-touch-icon" sizes="112x112" href="/favicon.png" />
-	<link rel="shortcut icon" href="/favicon.png" />
-	<title>Inferno Bells 🔔</title>
+  <link rel="apple-touch-icon" sizes="112x112" href="/favicon.png" />
+  <link rel="shortcut icon" href="/favicon.png" />
+  <title>Inferno Bells 🔔</title>
 </svelte:head>
 
 <ModeWatcher defaultMode="dark" />
